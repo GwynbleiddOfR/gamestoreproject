a3c67163b3544b47685dfd3b2a01eb8b955c9975
GwynbleiddOfR <se.apileo@duocuc.cl>
2024-06-20 01:19:35 -0400

    modelo Juego creado

--- gamewebstore/admin.py
+++ gamewebstore/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Juego
+
+class AdmJuego(admin.ModelAdmin):
+    list_display=['nomb_juego', 'genero', 'consola', 'precio', 'stock', 'descripcion', 'foto_juego']
+    list_filter=['consola']
 
 # Register your models here.
+admin.site.register(Juego, AdmJuego)
--- gamewebstore/models.py
+++ gamewebstore/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from .enumeraciones import *
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Juego(models.Model):
+    nomb_juego=models.CharField(max_length=100, null=False, verbose_name="Nombre")
+    genero=models.CharField(max_length=50, null=False)
+    consola=models.CharField(max_length=3, choices=TIPO_CONSOLA)
+    precio=models.PositiveIntegerField(default=0, validators=[MinValueValidator(0),MaxValueValidator(999999)])
+    stock=models.PositiveIntegerField(default=1, validators=[MinValueValidator(1),MaxValueValidator(99)])
+    descripcion=models.TextField()
+    foto_juego=models.ImageField(upload_to='juegos',null=False, verbose_name="Imagen")
