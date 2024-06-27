6b75e0e80610fe1b20c98c53a989be8d0bad37da
GwynbleiddOfR <se.apileo@duocuc.cl>
2024-06-26 21:51:16 -0400

    otros datos agregados a los templates

--- gamewebstore/forms.py
+++ gamewebstore/forms.py
@@ -5,7 +5,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Usuarios
 class UserForm(UserCreationForm):
-    pass
+    
+    class Meta:
+        model=User
+        fields=['username', 'first_name', 'last_name', 'email','password1','password2']
 
 class PerfilForm(forms.ModelForm):
 
